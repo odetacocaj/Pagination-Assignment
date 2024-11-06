a5cb83a628237d9f01c5cd40c2e973e5a50cdfbc
odetacocaj <odeta@solution25.com>
2024-11-06 16:39:25 +0100

    First commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
